346e33e106c3874cb257b5dbb89a466946adaaf2
cjgunase <cjgunase@mtu.edu>
2016-08-11 22:45:09 -0400

    Add other correlation methods

---
+++ SCCM/scripts3/test.pl
@@ -0,0 +1,2 @@
+use lib '/var/www/html/cluster/modules/share/perl5/';
+use Statistics::Basic qw(:all nofill);
---
+++ SCCM/scripts4/test.pl
@@ -0,0 +1,4 @@
+use lib '/var/www/html/cluster/modules/share/perl5/';
+
+use Statistics::Basic qw(:all nofill);
+use Statistics::LineFit;
